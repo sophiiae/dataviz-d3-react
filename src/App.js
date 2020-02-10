1f36ab732e348944027e9392b7ac57e7324adcb4
fei-fei.zheng@ibm.com <fei-fei.zheng@ibm.com@fei-2.war.can.ibm.com>
2020-02-10 15:54:35 -0500

    simple nav bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Switch, Route} from 'react-router-dom'; 
+import { 
+  BrowserRouter as Router,
+  Link,
+  Switch,
+  Route
+} from 'react-router-dom'; 
 import Shape from './basic/shape';
 import BarChart from './charts/BarChart'
 import BarChartSVG from './charts/BarChartCSV';
@@ -8,30 +13,26 @@ import ScatterChart from './charts/ScatterChart';
 import LineChart from './charts/LineChart';
 import WorldMap from './maps/WorldMap';
 import ChoroplethMap from './maps/ChoroplethMap'
-import { MenuList, MenuItem } from '@material-ui/core';
 import './App.css'
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <MenuList aria-label="breadcrumb">
-          <MenuItem>
-            <Link to="/">Home</Link>
-          </MenuItem>
-          <MenuItem>
-            <Link to="/charts">Charts</Link>
-          </MenuItem>
-          <MenuItem>
-            <Link to="/maps">Maps</Link>
-          </MenuItem>
-        </MenuList>
+        <div className="topnav">
+          <Link to="/charts">Charts</Link>
+          <Link to="/maps">Maps</Link>
+          <Link to="/rsm">RSM</Link>
+        </div>
         <Switch>
           <Route path="/charts">
             <BarChart />
           </Route>
           <Route path="/maps">
             {/* <WorldMap /> */}
+            <ChoroplethMap />
+          </Route>
+          <Route path="/rsm">
             <ChoroplethMap />
           </Route>
         </Switch>
